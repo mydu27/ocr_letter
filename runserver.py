ffe32a9cedb52152a59c4501ac38e7d280515ee8
unknown <594069557@qq.com>
2021-04-15 15:44:32 +0800

    提交初始文件

---
+++ runserver.py
@@ -0,0 +1,20 @@
+from app import create_app
+
+app = create_app()
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+
+    run_simple(hostname="0.0.0.0",
+               port=6889,
+               application=app,
+               use_reloader=True,
+               use_debugger=True,
+               use_evalex=True,
+               threaded=True)
